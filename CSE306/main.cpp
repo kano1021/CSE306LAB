b634e9e720d9cf1f7dcc1cee1a26fde67cadc244
kano1021 <36542884+kano1021@users.noreply.github.com>
2020-05-04 08:20:28 +0200

    Add files via upload

--- CSE306/main.cpp
+++ CSE306/main.cpp
@@ -13,20 +13,23 @@ int main() {
     int W = 512;
     int H = 512;
     double fov = 350;
-    int NB_path = 500;
+    int NB_path = 10;
     Scene scene;
+    Sphere *ball1=new Sphere(Vector(0, 0, 0), 10, Vector(1, 1, 1));
+    scene.add(ball1);
     Vector camera(0,0,55);
-    Image image=Image(W,H, fov, camera);
+    Image image=Image(W,H, fov);
     for (int i=0; i<H; i++) {
         for (int j=0; j<W; j++) {
             Vector V_O(j + 0.5 - W/2, i + 0.5 - H/2, 55 - fov);
             Vector V_u=normalize(V_O - camera);
             Ray ray(V_O, V_u); // cast a ray from the camera center to pixel i , j
-            Vector color(0,0,0);
+            Vector color(0.,0.,0.);
             for (int k=0;k<NB_path;k++){
                 color = color+ scene.getColor(ray, 2) ;
             }
             color/=NB_path;
+            cout<<"[" << color[0] << " "<< color[1] << " "<<color[2] <<"]"<<endl;
             image.pixel[i*W*3+j*3 + 0] = min(255, pow(color[0] , 1./2.2)*255); // stores R ←􏰀channel
             // same for green and blue
             image.pixel[i*W*3+j*3 + 1] = min(255, pow(color[1] , 1./2.2)*255); // stores G ←􏰀channel
