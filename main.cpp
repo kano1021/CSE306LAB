572b15df7247f8a439ea32e3b0512899b420dda0
kano1021 <36542884+kano1021@users.noreply.github.com>
2020-05-05 06:19:40 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -3,12 +3,9 @@
 #include "Methods/Scene.h"
 #include <iostream>
 #include <cmath>
+#include <chrono>
 
 using namespace std;
-double min(double a,double b){
-    if (a<b) return a;
-    else return b;
-}
 
 int main() {
 // first define the scene , variables , ... // then scan all pixels
@@ -16,7 +13,7 @@ int main() {
     int H = 512;
     double fov = M_PI/3;
     int NB_path = 10;
-    Scene scene;
+    Scene scene(true);
     Sphere *ball1=new Sphere(Vector(-20., 0., 0.), 10., Vector(1., 1., 1.),0);
     Sphere *ball2=new Sphere(Vector(0., 0., 0.), 10., Vector(1., 1., 1.),1.3);
     Sphere *ball3=new Sphere(Vector(20., 0., 0.), 10., Vector(1., 1., 1.),1.7);
@@ -27,19 +24,20 @@ int main() {
     Vector camera(0,0,55);
     Image image=Image(W,H, fov);
     //cout<<"Here 1"<<endl;
+    #pragma omp parallel for
     for (int i=0; i<H; i++) {
         for (int j=0; j<W; j++) {
+            double x, y;
             Vector V_O(j + 0.5 - W/2, i + 0.5 - H/2, 55 - W / (2 * tan(fov/ 2)));
             Vector V_u=normalize(V_O - camera);
             Ray ray(camera, V_u); // cast a ray from the camera center to pixel i , j
             Vector color(0.,0.,0.);
             //cout<<"Here 2"<<endl;
             for (int k=0;k<NB_path;k++){
+                boxMuller(1, x, y);
                 color = color+ scene.getColor(ray,5);
             }
-            //cout<<"[" << color[0] << " "<< color[1] << " "<<color[2] <<"]"<<endl;
             color/=NB_path;
-            //cout<<"[" << color[0] << " "<< color[1] << " "<<color[2] <<"]"<<endl;
             image.pixel[(H-i-1)*W*3+j*3 + 0] = max(0.,min(255., pow(color[0] , 1./2.2)*255)); // stores R ←􏰀channel
             // same for green and blue
             image.pixel[(H-i-1)*W*3+j*3 + 1] = max(0.,min(255., pow(color[1] , 1./2.2)*255)); // stores G ←􏰀channel
@@ -47,7 +45,7 @@ int main() {
             } 
         }
 // save image 
-    stbi_write_png("lab1_triball.png", W, H, 3, &image.pixel[0], 0);
+    stbi_write_png("triball without anti.png", W, H, 3, &image.pixel[0], 0);
     
     return 0;
 }
